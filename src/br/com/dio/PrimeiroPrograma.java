1d9d9318cc9532c5a3dc18b0ba3acc98751068ab
RogerioAguilera <raguileraribeiro33@gmail.com>
2022-03-09 06:09:39 -0400

    Add class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,5 +9,41 @@ public class PrimeiroPrograma {
         Leao leao = new Leao();
 
         System.out.println(leao);
+
+        Livro livro1 = new Livro ("O hobbit", 520);
+        System.out.println(livro1);
+
+    }
+}
+class Livro{
+    private String name;
+
+    public Livro(String o_hobbit, int i) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    private Integer numPaginas;
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
